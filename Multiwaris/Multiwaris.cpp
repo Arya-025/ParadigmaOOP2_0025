9d0a94ced5836e50643cd4d4c2b93be7d2e163ba
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-06-02 11:05:27 +0700

    revisi

---
+++ Multiwaris/Multiwaris.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+    public :
+    int umur ;
+
+    orang (int pUmur) :
+    umur(pUmur)
+    {
+        cout << "orang di buat dengan umur" << umur << "\n" << endl;
+    }
+
+};
+
+class pekerja : virtual public orang  {
+    public:
+    pekerja (int pUmur ) : orang (pUmur)
+    {
+        cout << "pekerja di buat \n" << endl;
+    }
+};
+
+class pelajar : virtual public orang{
+    public:
+    pelajar (int pUmur) :
+    orang(pUmur)
+    {
+        cout << "pelajar di buat \n" << endl;
+    }
+};
+
+class Budi : public pekerja, public pelajar {
+    public :
+    Budi (int pUmur):
+    pekerja (pUmur),
+    pelajar (pUmur),
+    orang (pUmur) //hal ini dapat di lakukan menggunakan virtual
+    {
+        cout << "budi di buat \n" << endl;
+    }
+
+};
+
+int main(){
+    Budi a(12);
+
+    return 0;
+}
