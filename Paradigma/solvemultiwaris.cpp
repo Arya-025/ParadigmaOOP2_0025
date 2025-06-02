beb126254d2e6517dea260743e602dee0d1e5ffb
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-06-02 11:03:00 +0700

    membuat solvemultiwaris

---
+++ Paradigma/solvemultiwaris.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+    public :
+    int umur ;
+
+    orang (int pUmur) :
+    umur(pUmur)
+    {
+        cout << "orang di buat dengan umur" << umur << "\n" << endl;
+    }
+
+};
+
+class pekerja : public orang  {
+    public:
+    pekerja (int pUmur ) : orang (pUmur)
+    {
+        cout << "pekerja di buat \n" << endl;
+    }
+};
+
+class pelajar : public orang{
+    public:
+    pelajar (int pUmur) :
+    orang(pUmur)
+    {
+        cout << "pelajar di buat \n" << endl;
+    }
+};
+
+class Budi : public pekerja, public pelajar {
+    public :
+    Budi (int pUmur):
+    pekerja (pUmur),
+    pelajar (pUmur),
+    {
+        cout << "budi di buat \n" << endl;
+    }
+
+};
+
+int main(){
+    Budi a(12);
+
+    return 0;
+}
