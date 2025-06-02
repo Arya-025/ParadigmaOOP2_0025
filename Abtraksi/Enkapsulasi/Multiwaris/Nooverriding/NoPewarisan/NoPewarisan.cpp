b17fa46b08804f3367fa49e9859da87af0e9a883
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-06-02 10:52:40 +0700

    mebuat class baseclass dan darived class

---
+++ Abtraksi/Enkapsulasi/Multiwaris/Nooverriding/NoPewarisan/NoPewarisan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+
+class baseClass{
+    public:
+    virtual void perkenalan (){
+        cout << "hallo saya function dari base class";
+    }
+};
+
+class darivedClass : public baseClass{
+    public :
+    void perkenalan (){
+        cout << "hallo saya function dari derived Class";
+    }
+};
