31dc6f4009cf0fd35cc3f7f6615d2be70a0c881f
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-06-02 09:56:15 +0700

    membuat class abstraksi

---
+++ Abtraksi/Abtraksi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Abstraksiklas {
+    private : string x, y;
+    //metod untuk mengisi nilai
+    //private member
+    public :
+    void setXY(string a, string b){
+        x = a;
+        y = b;
+    }
+}
