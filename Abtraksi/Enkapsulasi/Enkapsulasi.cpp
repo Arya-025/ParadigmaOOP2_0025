bf91ee2e75a40a6ce7484aefbcb44897851d0458
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-06-02 10:05:52 +0700

    membuat class dan modifier nya

--- Abtraksi/Enkapsulasi/Enkapsulasi.cpp
+++ Abtraksi/Enkapsulasi/Enkapsulasi.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+
+class remotelampu {
+    private :
+    string saklarNO[10];
+    public :
+    void setSaklarNO(int i, string value){
+        saklarNO [i] = value ;
+        
+    }
+}
