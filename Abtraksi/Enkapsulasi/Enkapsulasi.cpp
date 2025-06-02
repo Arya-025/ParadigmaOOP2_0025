d6fcc15b827f2b05ac182790ed171f06b712fbe8
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-06-02 10:14:54 +0700

    menampilkan hasil

--- Abtraksi/Enkapsulasi/Enkapsulasi.cpp
+++ Abtraksi/Enkapsulasi/Enkapsulasi.cpp
@@ -13,4 +13,20 @@ class remotelampu {
     string getsaklarNO(int i){
         return saklarNO[i];
     }
+};
+
+int main(){
+    remotelampu lampurumah;
+
+    lampurumah.setSaklarNO(0, "lampu teras rumah");
+    lampurumah.setSaklarNO(1, "lampu ruang tamu");
+    lampurumah.setSaklarNO(2, "lampu kamar tidur");
+    lampurumah.setSaklarNO(3, "lampu dapur");
+
+    cout << lampurumah.getsaklarNO(0)<< endl;
+    cout << lampurumah.getsaklarNO(1)<< endl;
+    cout << lampurumah.getsaklarNO(2)<< endl;
+    cout << lampurumah.getsaklarNO(3)<< endl;
+
+    return 0;
 }
